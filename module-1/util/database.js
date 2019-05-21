ba0504bb8d5f6817f8e2d6e625c3c3cf2246b48d
Dan Mackie <dan.mackie@gmail.com>
2019-05-21 14:21:44 +0100

    MongoDB NoSQL

--- module-1/util/database.js
+++ module-1/util/database.js
@@ -1,8 +1,28 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'd4nm4ck1320131972', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient
+    .connect('mongodb+srv://dan:d4nm4ck1320131972@cluster0-zabwv.gcp.mongodb.net/shop?retryWrites=true')
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback()
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    })
+}
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No DB found!!!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
